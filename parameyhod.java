0d0ef188cbe5590f5ffd28acd1783fc99de08737
rahul <rahulmandal705071@gmail.com>
2023-07-31 00:03:13 +1200

    jdhddh

---
+++ parameyhod.java
@@ -0,0 +1,11 @@
+public class parameyhod {
+    static void test(int roll, String student) {
+        System.out.println(roll + "is " + student);
+    }
+
+    public static void main(String args[]) {
+        test(1, "ram");
+        test(2, "noo");
+        test(3, "shiv");
+    }
+}
