4f0b69e66d06a3ffe3c4cb29cd9eb9410a39d3a4
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-07-31 23:44:23 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+public class add {
+    public static void main(String[] args) {
+        int a = 19;
+
+        int c = add(a, b);
+        System.out.println("The sum of a and b is= " + c);
+    }
+
+    public static int add(int n1, int n2) {
+        int s;
+        s = n1 + n2;
+        return s;
+    }
+}
